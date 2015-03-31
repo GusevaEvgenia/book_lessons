b8337732acf843d9382026e21b39784014a3c5be
Belka <ainege@gmail.com>
2015-03-31 14:53:02 +0300

    update MainActivity.java

--- GeoQuiz/app/src/main/java/com/lessons_book/belkin/geoquiz/MainActivity.java
+++ GeoQuiz/app/src/main/java/com/lessons_book/belkin/geoquiz/MainActivity.java
@@ -4,14 +4,40 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTrueButton = (Button)findViewById(R.id.trueButton);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseButton = (Button)findViewById(R.id.falseButton);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        R.string.correct_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
