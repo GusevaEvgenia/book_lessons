075b66f862f70754345217286ff56c3a4b9648d0
Belka <ainege@gmail.com>
2015-04-02 17:38:29 +0300

    Crime: add details, date to crime

--- Crime/app/src/main/java/com/android_lessons/belkin/crime/CrimeActivity.java
+++ Crime/app/src/main/java/com/android_lessons/belkin/crime/CrimeActivity.java
@@ -1,19 +1,19 @@
 package com.android_lessons.belkin.crime;
 
-import android.support.v4.app.Fragment;
+import android.support.v4.app.Fragment; //android.app.Fragment; без поддержки совместимости
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 
 
-public class CrimeActivity extends FragmentActivity {
+public class CrimeActivity extends FragmentActivity { //Activity ? без поддержки совместимости
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
 
-        FragmentManager fm = getSupportFragmentManager(); //getFragmentmanager(); без поддержки совместимости
+        FragmentManager fm = getSupportFragmentManager(); //getFragmentManager(); без поддержки совместимости
 
         Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
         if (fragment == null) {
--- Crime/app/src/main/java/com/android_lessons/belkin/crime/CrimeFragment.java
+++ Crime/app/src/main/java/com/android_lessons/belkin/crime/CrimeFragment.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 /**
@@ -17,6 +21,8 @@ public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,6 +50,18 @@ public class CrimeFragment extends Fragment {
             @Override
             public void afterTextChanged(Editable s) {
 
+            }
+        });
+
+        mDateButton = (Button)v.findViewById(R.id.crime_date);
+        mDateButton.setText(DateFormat.format("EEEE, MMM dd, yyyy", mCrime.getDate()));
+        mDateButton.setEnabled(false);
+
+       mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
             }
         });
 
