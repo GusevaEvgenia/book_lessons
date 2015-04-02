099776bd1487867b8036fa48781c2f23e366fdc5
Belka <ainege@gmail.com>
2015-04-02 14:44:44 +0300

    Crime: connect fragment to activity

--- Crime/app/src/main/java/com/android_lessons/belkin/crime/CrimeActivity.java
+++ Crime/app/src/main/java/com/android_lessons/belkin/crime/CrimeActivity.java
@@ -1,7 +1,9 @@
 package com.android_lessons.belkin.crime;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 
 
 public class CrimeActivity extends FragmentActivity {
@@ -10,5 +12,16 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager(); //getFragmentmanager(); без поддержки совместимости
+
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+
     }
 }
