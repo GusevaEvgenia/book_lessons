1045e07495d3d21d8f0a0e9e571a78074e4860e5
Belka <ainege@gmail.com>
2015-04-04 12:49:28 +0300

    Crime: add lost timePicker

---
+++ Crime/app/src/main/java/com/android_lessons/belkin/crime/TimePickerFragment.java
@@ -0,0 +1,86 @@
+package com.android_lessons.belkin.crime;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.View;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by Belkin on 04.04.2015.
+ */
+public class TimePickerFragment extends DialogFragment {
+
+    public static final String EXTRA_TIME = "com.bignerdranch.android.criminalintent.time";
+    private Date mDate;
+
+    public static TimePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(EXTRA_TIME, date);
+        TimePickerFragment fragment = new TimePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    private void sendResult(int resultCode) {
+        if (getTargetFragment() == null)
+            return;
+
+        Intent i = new Intent();
+        i.putExtra(EXTRA_TIME, mDate);
+
+        getTargetFragment().onActivityResult(getTargetRequestCode(), resultCode, i);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        mDate = (Date)getArguments().getSerializable(EXTRA_TIME);
+
+        // создание объекта Calendar для получения года, месяца и дня
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(mDate);
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_time, null);
+
+        TimePicker timePicker = (TimePicker)v.findViewById(R.id.dialog_time_timePicker);
+        timePicker.setIs24HourView(true);
+        timePicker.setCurrentHour(hour);
+        timePicker.setCurrentMinute(minute);
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                // Преобразование года, месяца и дня в объект Date
+                mDate = new GregorianCalendar(year, month, day, hourOfDay, minute).getTime();
+                // обновление аргумента для сохранения
+                // выбранного значения при повороте
+                getArguments().putSerializable(EXTRA_TIME, mDate);
+            }
+        });
+
+        return new AlertDialog.Builder(getActivity())
+                .setView(v)
+                .setTitle(R.string.time_picker_title)
+                .setPositiveButton(
+                        android.R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                sendResult(Activity.RESULT_OK);
+                            }
+                        })
+                .create();
+    }
+}
